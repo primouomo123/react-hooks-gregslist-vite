35f306a0e5b3a3ee26e7714e18433743a3dd4814
Laura Berge <lauraeberge@gmail.com>
2025-02-20 16:48:26 -0600

    add form styling and render to page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./Header";
+import ListingForm from "./ListingForm";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <ListingForm />
       <ListingsContainer />
     </div>
   );
